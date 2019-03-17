5e6d5c07e94d4e61714d3767a99a1f4586c74b16
Evdokia Mina <minaE@cardiff.ac.uk>
2019-03-17 20:24:35 +0000

    no important changes

--- HomeHelpService.py
+++ HomeHelpService.py
@@ -12,9 +12,9 @@ rnd = np.random
 # rnd.seed(2734086387)
 # rnd.seed(2278517282)
 # rnd.seed(3933075158)
-# rnd.seed(4214019853)
-# rnd.seed(3345479024)
-rnd.seed(3761229082) # doesnt work
+rnd.seed(4214019853)
+# rnd.seed(3345479024) # doesnt work
+# rnd.seed(3761229082) # doesnt work
 # rnd.seed(3316967774) #doesnt work
 # rnd.seed(3636448687) #doesnt work
 # rnd.seed(2387376734)
@@ -52,6 +52,8 @@ x=m.addVars(A, obj=c, vtype=GRB.BINARY, name='x')
 for i,j in x.keys():
   x[j,i] = x[i,j] # edge in opposite direction
 m.update()
+u=m.addVars(N, ub=Q, vtype=GRB.INTEGER, name='u')
+m.update()
 
 #minimize problem 
 m.setObjective(quicksum(c[i,j]*x[i,j] for i,j in A ), GRB.MINIMIZE)
@@ -59,22 +61,23 @@ m.setObjective(quicksum(c[i,j]*x[i,j] for i,j in A ), GRB.MINIMIZE)
 #constraints
 # con1 = m.addConstrs(x.sum(i,'*') == 2 for i in N)
 for i in N:
-  m.addConstr(quicksum(x[i,j] for j in V if i!=j) ==2)
+  m.addConstr(quicksum(x[i,j] for j in V if i!=j) ==2, 'con1')
 m.update()
 # for i in E:
 #   m.addConstr(quicksum(x[i,j] for j in V if i!=j) ==2)
 # m.update()
-# # con3 = m.addConstrs(x.sum(i,'*') == 2 for i in E)
-# con3 = m.addConstrs(quicksum(x[i,j] for i in N if i!=j)==2 for j in V)
-# con3 = m.addConstrs(quicksum(x[i,j] for i in E if i!=j)==2 for j in V)
+# for i in E:
+#   m.addConstr(quicksum(x[i,j] for j in V if i!=j) ==1)
+# m.update()
+# con3 = m.addConstrs(x.sum(i,'*') == 2 for i in E)
+# con3 = m.addConstrs(quicksum(x[i,j] for i in N if i!=j)==1 for j in V)
 # con3 = m.addConstrs(quicksum(x[i,j] for i in V if i!=j)==1 for j in N)
-# con3 = m.addConstrs(quicksum(x[i,j] for i in E if i!=j)<=1 for j in V)
-# con3 = m.addConstrs(quicksum(x[j,i] for i in E if i!=j)<=1 for j in V)
+# con3 = m.addConstrs(quicksum(x[i,j] for i in E if i!=j)<=2 for j in V)
+# con3 = m.addConstrs(quicksum(x[i,j] for i in V if i!=j)<=1 for j in E)
 con2 = m.addConstrs((x[i,j] == 1 ) >> (x[i,j] == 0) for i,j in A if i in E and j in E and i!=j )
-# con2 = m.addConstrs((x[i,j] == 1 ) >> (x[i,j] == 0) for i,j in A if i in E and j in N and i!=j )
 # con3 = m.addConstrs((x[i,j] == 1) >> (u[j]== u[i] + 1) for i,j in A if i!=j and i not in E and j not in E)
 # con4 = m.addConstrs((x[i,j] == 1) >> (u[i]-u[j]+(Q*x[i,j])==Q-1) for i,j in A if i not in E and j not in E and i!=j)
-# con5 = m.addConstrs(u[i]<=Q for i in N)
+# con5 = m.addConstrs(u[i]>=Q for i in N)
 
 
 def subtour(edges):
@@ -140,17 +143,20 @@ def subtourelim(model, where):
       if len(I) == 0 and len(t)>1:
         print('no empoloyees')
         model.cbLazy(quicksum(x[i,j] for i,j in itertools.combinations(t, 2))<= len(t)-1)
-      if len(I)==2 and len(SUnion) == 1:
+      elif len(I)==2 and len(SUnion) == 1:
         model.cbLazy(x[I[0],SUnion[0]] + quicksum(x[i,SUnion[0]] for i in E if i!=I[0])<=1)
-      if len(S)>0 and len(I)==2:
+      elif len(S)>0 and len(I)==2:
         print('more than 1 employee, more than 2 clients')
         # model.cbLazy(x[I[0],SUnion[0]] + 2*quicksum(x[i,j] for i,j in itertools.combinations(SUnion, 2))+ quicksum(x[i,SUnion[len(SUnion)-1]] for i in E if i!=I[0])<=2*len(S)+3)
-        model.cbLazy(x[I[0],SUnion[0]] + quicksum(x[i,SUnion[len(SUnion)-1]] for i in E if i!=I[0])<=1)
-      if len(SUnion) == 2 and len(I)==2:
+        model.cbLazy(x[I[0],SUnion[0]] + quicksum(x[i,j] for i in E for j in SUnion if i!=I[0])<=1)
+      elif len(SUnion) == 2 and len(I)==2:
         print('only 2 clients and 2 employees')
         # model.cbLazy(x[I[0],SUnion[0]] + quicksum(x[i,SUnion[1]] for i in E if i!=I[0])<=1)
-        model.cbLazy(x[I[0],SUnion[0]] + 3*x[SUnion[0],SUnion[1]] + quicksum(x[i,SUnion[1]] for i in E if i!=I[0])<= 4)    
-  
+        model.cbLazy(x[I[0],SUnion[0]] + 3*x[SUnion[0],SUnion[1]] + quicksum(x[i,SUnion[1]] for i in E if i!=I[0])<= 4) 
+      if len(t)>=2:
+        if model.cbGetSolution(x[t[0],t[len(t)-1]]) == 0:
+          print('tour not connected')
+          model.cbLazy(quicksum(x[t[0],t[len(t)-1]]) >=1)
 # Optimize
 m._vars = m.getVars()
 m.params.LazyConstraints = 1
@@ -159,8 +165,8 @@ m.params.LazyConstraints = 1
 # m.params.ZerohalfCuts = 0
 # m.params.Threads = 1
 # m.params.PreCrush = 1
-# m.params.DualReductions = 0
-# m.params.Cuts = 2
+# m.params.CutAggPasses = 2
+# m.params.Cuts = 0
 # m.params.Heuristics = 0
 m.optimize(subtourelim)
 # m.optimize()
@@ -185,7 +191,7 @@ if status != GRB.Status.INF_OR_UNBD and status != GRB.Status.INFEASIBLE:
     exit(0)
 
 print('The model is infeasible; relaxing the constraints')
-m.feasRelaxS(0, False, False, True)
+# m.feasRelaxS(0, False, False, True)
 
 if status in (GRB.Status.INF_OR_UNBD, GRB.Status.INFEASIBLE, GRB.Status.UNBOUNDED):
     print('The relaxed model cannot be solved \
